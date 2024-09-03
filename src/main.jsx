46b1ace182ec37d04c1b5c648d02b325f627a31e
BigDahn <harkinborholarholuwah@gmail.com.com>
2024-09-03 23:56:37 +0100

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,10 @@ import App from './MultiForm/App'
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
-import { Provider } from 'react-redux'
-import {  store } from './Bookmark/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <Provider store={store}>
-      
-        <App />
-        <ToastContainer position="top-center" />
-      
-    </Provider>
+    <App />
+    <ToastContainer position="top-center" />
   </>
 )
